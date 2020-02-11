4f8484a7dd1a8e21d55eb6e8a973b3f1006c42aa
Gabriel <tarquinio34@gmail.com>
2020-02-11 09:46:22 -0300

    codigo chico funcionando

--- CODIGO/CHICO/main.c
+++ CODIGO/CHICO/main.c
@@ -34,82 +34,84 @@
 
 struct nrf24l01p nrf;
 uint8_t addresses[][6] = {"1Node","2Node"};
-
+//int i;
 
 
 int main(void) {
 
-    uint8_t ui8LED = 2;
-	//
-	// Enable lazy stacking for interrupt handlers.  This allows floating-point
-	// instructions to be used within interrupt handlers, but at the expense of
-	// extra stack usage.
-	//
-	FPULazyStackingEnable();
-	FPUEnable();
+  //  uint8_t ui8LED = 2;
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    FPULazyStackingEnable();
+    FPUEnable();
 
 
-	setup();
+    setup();
 
-	IntMasterEnable();
+    IntMasterEnable();
 
-	led_pin_config();
+    led_pin_config();
 
-	//set up the radio
-	if( !nrf24l01p_setup(&nrf, GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_PORTC_BASE, GPIO_PIN_4, SSI0_BASE) ){
-		//we couldn't communicate with the radio
-		//return 1;
-	}
+    //set up the radio
+    if( !nrf24l01p_setup(&nrf, GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_PORTC_BASE, GPIO_PIN_4, SSI0_BASE) ){
+        //we couldn't communicate with the radio
+        //return 1;
+    }
 
-	nrf24l01p_set_PA_level(&nrf, RF24_PA_LOW);
+    nrf24l01p_set_PA_level(&nrf, RF24_PA_LOW);
 
-	nrf24l01p_open_writing_pipe(&nrf, addresses[0]);
-	nrf24l01p_open_reading_pipe(&nrf, 1,addresses[1]);
+    nrf24l01p_open_writing_pipe(&nrf, addresses[0]);
+    nrf24l01p_open_reading_pipe(&nrf, 1,addresses[1]);
 
-	nrf24l01p_start_listening(&nrf);
+   // nrf24l01p_start_listening(&nrf);
+    uint8_t recebido[6];
+    uint8_t envio[] = { ' ','M','A','R','A', 'C','A'} ;
+    while(1){
+    //    i = 0;
+    //   while (i < 6) {
+       nrf24l01p_stop_listening(&nrf);        // First, stop listening so we can talk
+        nrf24l01p_write(&nrf, &envio, sizeof(envio)); // Send the final one back.
+       nrf24l01p_start_listening(&nrf); // Now, resume listening so we catch the next packets.
 
-	while(1){
-		if (nrf24l01p_available(&nrf)) {
-			uint8_t recebido;
-			uint8_t envio;
-			// Variable for the received time stamp
-			while (nrf24l01p_available(&nrf)) {                 // While there is data ready
-				nrf24l01p_read(&nrf, &recebido, sizeof(uint8_t));     // Get the payload
-			}
+        if (nrf24l01p_available(&nrf)) {
+  //          uint8_t recebido[6];
+  //          uint8_t envio[6] = {'M','A','R','A', 'C','A'} ;
+            // Variable for the received time stamp
 
-			nrf24l01p_stop_listening(&nrf);        // First, stop listening so we can talk
-			nrf24l01p_write(&nrf, &envio, sizeof(uint8_t)); // Send the final one back.
-			nrf24l01p_start_listening(&nrf); // Now, resume listening so we catch the next packets.
-
-			if(recebido==0x0F){
-			    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2, 255);
-			}else{
-			    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2, 0);
-			}
-
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_3, 0);
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2, 255);
+              //  SysCtlDelay(5000000);
+                nrf24l01p_read(&nrf, &recebido, sizeof(recebido));     // Get the payload
 
 
-		}
+      //      nrf24l01p_stop_listening(&nrf);        // First, stop listening so we can talk
+      //      nrf24l01p_write(&nrf, &envio, sizeof(envio)); // Send the final one back.
+       //     nrf24l01p_start_listening(&nrf); // Now, resume listening so we catch the next packets.
+       //         ++i ;
 
-//		//give it a little space
-//		SysCtlDelay((SysCtlClockGet() >> 12) * 5);
-//
-//		// Turn on the LED
-//		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, ui8LED);
-//		// Delay for a bit
-//		SysCtlDelay(10000000);
-//
-//		// Cycle through Red, Green and Blue LEDs
-//		if (ui8LED == 8+4+2)
-//		{
-//		     ui8LED = 2;
-//		}
-//		else
-//		{
-//
-//		    ++ui8LED;
-//		}
-	}
+      //  }
 
-	//return 0;
+            if(recebido[0]==envio[1]){
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2, 0);
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3, 255);
+               // SysCtlDelay(5000000);
+
+            }else{
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2, 0);
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1, 255);
+               // SysCtlDelay(20000000);
+            }
+
+        }
+        }
+
+//      //give it a little space
+//      SysCtlDelay((SysCtlClockGet() >> 12) * 5);
+
+
+
+    //return 0;
 }
